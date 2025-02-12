52544c4afc4c087f91e436417e41f0d5bb36b7f1
LyrinePoliarco <152787755+LyrinePoliarco@users.noreply.github.com>
2025-02-12 14:36:52 +0800

    Update ClosedState.java

--- ClosedState.java
+++ ClosedState.java
@@ -12,15 +12,18 @@ public class ClosedState implements AccountState {
     @Override
     public void suspend(Account account) {
         System.out.println("You cannot suspend a closed account!");
+        
     }
 
     @Override
     public void activate(Account account) {
         System.out.println("You cannot activate a closed account!");
+       
     }
 
     @Override
     public void close(Account account) {
         System.out.println("Account is already closed!");
+       
     }
 }
