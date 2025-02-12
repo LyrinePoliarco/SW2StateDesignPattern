c535283a844ca49716e8bc8a794841a2ccb2f4e9
LyrinePoliarco <152787755+LyrinePoliarco@users.noreply.github.com>
2025-02-12 14:36:24 +0800

    Update ActiveState.java

--- ActiveState.java
+++ ActiveState.java
@@ -1,34 +1,28 @@
-public class ActiveState implements AccountState {
-    @Override
-    public void deposit(Account account, double amount) {
-        account.setBalance(account.getBalance() + amount);
-        System.out.println("Deposited " + amount + ". New balance: " + account.getBalance());
+public class ActivateState implements AccountState{ 
+
+    @Override 
+    public void deposit(Account account, double amount) 
+    { 
+        account.setBalance(account.getBalance() + amount); 
+    } 
+    @Override 
+    public void withdraw(Account account, double amount)
+     { 
+        account.setBalance(account.getBalance() - amount); 
+    } 
+    @Override 
+    public void activate(Account account){ 
+        System.out.println("Account is already activated!"); 
+    } 
+    @Override 
+     public void suspend(Account account){ 
+        System.out.println( "Account is suspended!"); 
+     account.setAccountState(new SuspendedState());
+    } 
+    @Override 
+    public void close(Account account){ 
+        System.out.println("Account is closed!"); 
+     account.setAccountState(new ClosedState());
+     } 
     }
 
-    @Override
-    public void withdraw(Account account, double amount) {
-        if (amount <= account.getBalance()) {
-            account.setBalance(account.getBalance() - amount);
-            System.out.println("Withdrew " + amount + ". New balance: " + account.getBalance());
-        } else {
-            System.out.println("Insufficient funds for withdrawal!");
-        }
-    }
-
-    @Override
-    public void suspend(Account account) {
-        account.setState(new SuspendedState());
-        System.out.println("Account is suspended!");
-    }
-
-    @Override
-    public void activate(Account account) {
-        System.out.println("Account is already activated!");
-    }
-
-    @Override
-    public void close(Account account) {
-        account.setState(new ClosedState());
-        System.out.println("Account is closed!");
-    }
-}
